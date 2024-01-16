4a81a85f2c99832ea38df66a1ef4a5b165105337
Ibannng <balawansatria123@gmail.com>
2024-01-16 14:06:14 +0700

    First Commit

---
+++ models/quiz.js
@@ -0,0 +1,31 @@
+const { Sequelize } = require("sequelize");
+
+module.exports = (sequalize, Sequelize) => {
+  const Quiz = sequalize.define("quiz", {
+    quiz: {
+      type: Sequelize.STRING,
+    },
+    a: {
+      type: Sequelize.STRING,
+    },
+    b: {
+      type: Sequelize.STRING,
+    },
+    c: {
+      type: Sequelize.STRING,
+    },
+    d: {
+      type: Sequelize.STRING,
+    },
+    key: {
+      type: Sequelize.STRING,
+    },
+    categoryId: {
+      type: Sequelize.INTEGER,
+    },
+    levelId: {
+      type: Sequelize.INTEGER,
+    },
+  });
+  return Quiz;
+};
